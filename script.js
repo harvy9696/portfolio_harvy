22bc894be8a4198d3e39c9f373b92fee90f80404
harvy9696 <hkkbelgera@mcm.edu.ph>
2026-06-26 18:02:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const button = document.getElementById("themeToggle");
+const themeButton = document.getElementById("themeToggle");
 
-button.addEventListener("click", () => {
+themeButton.addEventListener("click", () => {
+
     document.body.classList.toggle("dark-mode");
+
+    if(document.body.classList.contains("dark-mode")){
+
+        themeButton.textContent = "☀️ Light Mode";
+
+    }else{
+
+        themeButton.textContent = "🌙 Dark Mode";
+
+    }
+
 });
